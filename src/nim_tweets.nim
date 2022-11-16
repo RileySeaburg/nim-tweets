c45fb23e7c9ee89356d56147e91d91e29cf093ec
RileySeaburg <riley@rileyseaburg.com>
2022-11-16 07:09:33 -0500

    auto redirect user with cookie

--- src/nim_tweets.nim
+++ src/nim_tweets.nim
@@ -1,12 +1,20 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 import asyncdispatch, times, database, views/user, views/general
-
 import jester
+
+let db = newDatabase()
 routes: 
   get "/":
-    resp renderMain(renderLogin())
-    
+    if request.cookies.hasKey("username"):
+      var user: User
+      if not db.findUser(request.cookies["username"], user):
+        user = User(username: request.cookies["username"], following: @[])
+        db.create(user)
+      let messages = db.findMessages(user.following)
+      resp renderMain(renderTimeline(user.username, messages))
+    else:
+      resp renderMain(renderLogin())
   post "/login":
     setCookie("username", @"username", now() + 2.hours)
     redirect("/")
