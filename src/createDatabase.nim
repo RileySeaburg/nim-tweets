6e43e294adb2f7d97629d95c1f3d35d833d8086d
RileySeaburg <riley@rileyseaburg.com>
2022-11-16 02:41:08 -0500

    create sqlite db

--- src/createDatabase.nim
+++ src/createDatabase.nim
@@ -1,14 +1,33 @@
 import  db_sqlite
 
-var db = open("nim_tweets.db". "", "", "")
+var db = open("nim_tweets.db", "", "", "")
 
+
+db.exec(sql"""
+  CREATE TABLE IF NOT EXISTS User(
+    username text PRIMARY KEY
+  );
+""")
 
 db.exec(sql"""
   CREATE TABLE IF NOT EXISTS Following(
     follower text,
     followed_user text,
-    PRIMARY_KEY ()
-  )
-)
+    PRIMARY KEY (follower, followed_user)
+    FOREIGN KEY (follower) REFERENCES User(username),
+    FOREIGN KEY (followed_user) REFERENCES User(username)
+  );
+""")
+
+db.exec(sql"""
+  CREATE TABLE IF NOT EXISTS Message(
+    username text,
+    time integer,
+    msg text NOT NULL,
+    FOREIGN KEY (username) REFERENCES User(username)
+  );
+""")
+
+echo("Database created sucessfully!")
 
 db.close()
